e7cdcfb8a94e2b81ce34b649a196115199463deb
Dave Vasilevsky <dave@vasilevsky.ca>
2022-12-04 00:20:51 -0500

    day3-2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error;
 use std::fs;
 use std::io::{self, BufRead};
@@ -7,7 +7,7 @@ use std::str::FromStr;
 
 type BoxResult<T> = Result<T, Box<dyn error::Error>>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Input {
     a: Vec<char>,
     b: Vec<char>,
@@ -38,18 +38,6 @@ impl FromStr for Input {
 }
 
 impl Input {
-    fn parse(path: String) -> BoxResult<Vec<Input>> {
-        let file = fs::File::open(path)?;
-        let lines: Vec<String> = io::BufReader::new(file)
-            .lines()
-            .collect::<io::Result<Vec<String>>>()?;
-        let parsed: Vec<Input> = lines
-            .iter()
-            .map(|l| l.parse::<Input>())
-            .collect::<Result<Vec<Input>, InputErr>>()?;
-        Ok(parsed)
-    }
-
     fn priority(c: char) -> u32 {
         if c.is_lowercase() {
             c.to_digit(36).unwrap() - 10 + 1
@@ -65,17 +53,64 @@ impl Input {
         **c
     }
 
-    fn part1(inputs: &Vec<Input>) -> u32 {
-        inputs
+    fn all(&self) -> HashSet<&char> {
+        self.a.iter().chain(self.b.iter()).collect::<HashSet<_>>()
+    }
+}
+
+struct Inputs(Vec<Input>);
+
+impl Inputs {
+    fn parse(path: String) -> BoxResult<Self> {
+        let file = fs::File::open(path)?;
+        let lines: Vec<String> = io::BufReader::new(file)
+            .lines()
+            .collect::<io::Result<Vec<String>>>()?;
+        let parsed: Vec<Input> = lines
+            .iter()
+            .map(|l| l.parse::<Input>())
+            .collect::<Result<Vec<Input>, InputErr>>()?;
+        Ok(Inputs(parsed))
+    }
+
+    fn part1(&self) -> u32 {
+        self.0
             .iter()
             .map(|i| Input::priority(i.common()))
             .sum::<u32>()
     }
+
+    fn groups(&self) -> Vec<Group> {
+        self.0.chunks(3).map(|g| Group(g.to_vec())).collect()
+    }
+
+    fn part2(&self) -> u32 {
+        self.groups()
+            .iter()
+            .map(|g| Input::priority(g.badge()))
+            .sum()
+    }
+}
+
+struct Group(Vec<Input>);
+
+impl Group {
+    fn badge(&self) -> char {
+        let mut counts = HashMap::new();
+        for input in &self.0 {
+            for c in input.all() {
+                *counts.entry(*c).or_insert(0) += 1;
+            }
+        }
+
+        *counts.iter().find(|e| *e.1 == 3).unwrap().0
+    }
 }
 
 fn main() -> BoxResult<()> {
-    let input = Input::parse(std::env::args().nth(1).expect("need file"))?;
-    println!("{:?}", Input::part1(&input));
+    let inputs = Inputs::parse(std::env::args().nth(1).expect("need file"))?;
+    println!("Part 1: {}", inputs.part1());
+    println!("Part 2: {}", inputs.part2());
 
     Ok(())
 }
