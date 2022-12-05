1fc5b25bb82a887abe7c54afcc8b8d10a8b03b52
Dave Vasilevsky <dave@vasilevsky.ca>
2022-12-05 07:54:28 -0500

    day5-1

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,139 +1,101 @@
 use regex::Regex;
-use std::error::Error;
-use std::fmt::{Display, Formatter};
 use std::fs;
-use std::str::FromStr;
-
-type BoxErr = Box<dyn Error>;
-type BoxResult<T> = Result<T, BoxErr>;
 
 #[derive(Debug)]
-struct ParseError(String);
-
-impl ParseError {
-    fn new(s: &str) -> BoxErr {
-        Box::new(ParseError(s.to_owned()))
-    }
-}
-
-impl Display for ParseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&*self.0)
-    }
-}
-
-impl Error for ParseError {}
-
-#[derive(Debug, Clone)]
 struct Stack(Vec<char>);
-
-impl Stack {
-    fn new() -> Self {
-        Stack(Vec::new())
-    }
-}
 
 #[derive(Debug)]
 struct Stacks(Vec<Stack>);
 
-impl FromStr for Stacks {
-    type Err = BoxErr;
+impl Stacks {
+    fn stack(&mut self, one_idx: usize) -> &mut Stack {
+        self.0.get_mut(one_idx - 1).expect("no such stack")
+    }
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let lines = s.split("\n").collect::<Vec<_>>();
-        let mut rev = lines.iter().rev();
-        let first = rev.next().ok_or(ParseError::new("no stack lines"))?;
-
-        let len = first.split_whitespace().count();
-        let mut stacks = Vec::new();
-        stacks.resize(len, Stack::new());
-        for line in rev {
-            let chars = line.chars().collect::<Vec<_>>();
-            for (idx, st) in stacks.iter_mut().enumerate() {
-                let chr_idx = 1 + 4 * idx;
-                if let Some(chr) = chars.get(chr_idx) {
-                    if !chr.is_whitespace() {
-                        st.0.push(*chr);
-                    }
-                }
-            }
+    fn do_move(&mut self, mv: &Move) {
+        for _ in 0..mv.count {
+            let c = self.stack(mv.source).0.pop().expect("non-empty source");
+            self.stack(mv.dest).0.push(c);
         }
-
-        Ok(Stacks(stacks))
     }
 }
 
 #[derive(Debug)]
 struct Move {
     count: i32,
-    // one-indexed
-    source: i32,
-    dest: i32,
-}
-
-impl Move {
-    fn parse_int(match_opt: Option<regex::Match>) -> BoxResult<i32> {
-        let m = match_opt.ok_or(ParseError::new("no capture"))?;
-        let i = m.as_str().parse::<i32>()?;
-        Ok(i)
-    }
-}
-
-impl FromStr for Move {
-    type Err = BoxErr;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        println!("{}", s);
-        let re = Regex::new(r"\Amove (\d+) from (\d+) to (\d+)\z")?;
-        let caps = re.captures(s).ok_or(ParseError::new("no match"))?;
-        let count = Move::parse_int(caps.get(1))?;
-        let source = Move::parse_int(caps.get(2))?;
-        let dest = Move::parse_int(caps.get(3))?;
-        Ok(Move {
-            count: count,
-            source: source,
-            dest: dest,
-        })
-    }
+    // one-based indexes
+    source: usize,
+    dest: usize,
 }
 
 #[derive(Debug)]
-struct Inputs {
+struct Input {
     stacks: Stacks,
     moves: Vec<Move>,
 }
 
-impl FromStr for Inputs {
-    type Err = BoxErr;
+impl Input {
+    fn parse(file: String) -> Input {
+        let mut input = Input {
+            stacks: Stacks(Vec::new()),
+            moves: Vec::new(),
+        };
 
-    fn from_str(data: &str) -> BoxResult<Self> {
-        let (stack_part, moves_part) = data
-            .split_once("\n\n")
-            .ok_or(ParseError::new("no delimiter"))?;
-        let stacks = stack_part.parse::<Stacks>()?;
-        let moves: Vec<Move> = moves_part
-            .split("\n")
-            .filter(|line| !line.is_empty())
-            .map(|line| line.parse::<Move>())
-            .collect::<Result<Vec<_>, _>>()?;
-        Ok(Inputs {
-            stacks: stacks,
-            moves: moves,
-        })
+        let contents = fs::read_to_string(file).expect("read file");
+        let (stack_part, move_part) = contents.split_once("\n\n").expect("split file");
+        let mut rev = stack_part.lines().rev();
+
+        let header = rev.next().expect("header");
+        let stack_count = header.split_whitespace().count();
+        input
+            .stacks
+            .0
+            .resize_with(stack_count, || Stack(Vec::new()));
+
+        for line in rev {
+            let chars = line.chars().collect::<Vec<_>>();
+            for (i, st) in input.stacks.0.iter_mut().enumerate() {
+                if let Some(chr) = chars.get(1 + i * 4) {
+                    if chr.is_alphabetic() {
+                        st.0.push(*chr);
+                    }
+                }
+            }
+        }
+
+        let move_re = Regex::new(r"\Amove (\d+) from (\d+) to (\d+)\z").expect("regex compile");
+        for line in move_part.lines().filter(|l| !l.is_empty()) {
+            let caps = move_re.captures(line).expect("regex match");
+            input.moves.push(Move {
+                count: caps[1].parse::<i32>().expect("parse count"),
+                source: caps[2].parse::<usize>().expect("parse source"),
+                dest: caps[3].parse::<usize>().expect("parse dest"),
+            })
+        }
+
+        input
+    }
+
+    fn execute(&mut self) {
+        for m in &self.moves {
+            self.stacks.do_move(&m);
+        }
+    }
+
+    fn part1(&mut self) -> String {
+        self.execute();
+        let tops = self
+            .stacks
+            .0
+            .iter()
+            .map(|s| s.0.last().expect("non-empty").to_string())
+            .collect::<Vec<_>>();
+        tops.join("")
     }
 }
 
-impl Inputs {
-    fn parse(path: String) -> BoxResult<Self> {
-        let data = fs::read_to_string(path)?;
-        data.parse::<Inputs>()
-    }
-}
-
-fn main() -> BoxResult<()> {
+fn main() {
     let file = std::env::args().nth(1).expect("need file");
-    let inputs = Inputs::parse(file)?;
-    println!("{:?}", inputs);
-
-    Ok(())
+    let mut input = Input::parse(file);
+    println!("Part 1: {}", input.part1());
 }
