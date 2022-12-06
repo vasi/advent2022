3fdc981f1f6a1a7f34b2402209f3f644a2ecb1d3
Dave Vasilevsky <dave@vasilevsky.ca>
2022-12-06 07:00:34 -0500

    day6-2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,14 +2,16 @@ use std::collections::{HashMap, VecDeque};
 use std::fs;
 
 struct Stats {
+    target: usize,
     total: usize,
     deq: VecDeque<char>,
     counts: HashMap<char, usize>,
 }
 
 impl Stats {
-    fn new() -> Self {
+    fn new(target: usize) -> Self {
         Stats {
+            target: target,
             total: 0,
             deq: VecDeque::new(),
             counts: HashMap::new(),
@@ -17,21 +19,21 @@ impl Stats {
     }
 
     fn complete(&self) -> bool {
-        self.total >= 4 && self.counts.iter().all(|(_, v)| *v <= 1)
+        self.total >= self.target && self.counts.iter().all(|(_, v)| *v <= 1)
     }
 
     fn add(&mut self, c: char) {
         self.total += 1;
         self.deq.push_back(c);
         *self.counts.entry(c).or_insert(0) += 1;
-        if self.deq.len() > 4 {
+        if self.deq.len() > self.target {
             let d = self.deq.pop_front().unwrap();
             *self.counts.entry(d).or_insert(0) -= 1
         }
     }
 
-    fn part1(s: String) -> usize {
-        let mut stats = Stats::new();
+    fn solve(s: &str, target: usize) -> usize {
+        let mut stats = Stats::new(target);
         for c in s.chars() {
             stats.add(c);
             if stats.complete() {
@@ -45,5 +47,6 @@ impl Stats {
 fn main() {
     let arg = std::env::args().nth(1).expect("need arg");
     let buffer = fs::read_to_string(&arg).unwrap_or(arg);
-    println!("Part 1: {}", Stats::part1(buffer));
+    println!("Part 1: {}", Stats::solve(&buffer, 4));
+    println!("Part 2: {}", Stats::solve(&buffer, 14));
 }
